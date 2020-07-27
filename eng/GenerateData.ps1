05604655d5d7fc9e96ef15c5fae9d35edfd97ac1
Michael Randers-Pehrson <michael.rp@gmail.com>
2020-07-27 18:07:53 -0400

    Tags should be array

--- eng/GenerateData.ps1
+++ eng/GenerateData.ps1
@@ -14,7 +14,7 @@ foreach ($file in $postMdFiles)
 {
     $metadata = Get-Content $file -First 3
     $title = $metadata[0].Substring(6).Trim()
-    $tags = $metadata[1].Substring(5).Replace(", ", ",").Trim()
+    $postTags = $metadata[1].Substring(5).Split(",").Trim()
     $date = $metadata[2].Substring(5).Trim()
 
     $label = $file | Select-Object -ExpandProperty BaseName
@@ -24,7 +24,7 @@ foreach ($file in $postMdFiles)
     $post = Select-Object `
         @{ Name = "Index"; Expression={$index} }, `
         @{n='Label';e={$label}}, `
-        @{n='Tags';e={$tags}}, `
+        @{n='Tags';e={$postTags}}, `
         @{n='Title';e={$title}}, `
         @{n='Date';e={$date}}, `
         @{n='Content';e={$mainContent}} `
@@ -51,7 +51,7 @@ foreach ($file in $postJsonFiles)
 
     $r = $posts.Add($indexPost)
 
-    $postTags = $postWithContent.Tags.Split(',')
+    $postTags = $postWithContent.Tags
     foreach ($tag in $postTags) {
         $tagHash[$tag] += 1
     }
